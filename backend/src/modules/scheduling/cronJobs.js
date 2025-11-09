61f72b8eee2184c3eb02aa36755e436787a4433b
tarshalim <e1155487@u.nus.edu>
2025-11-09 20:33:20 +0800

    fixed routing

--- backend/src/modules/scheduling/cronJobs.js
+++ backend/src/modules/scheduling/cronJobs.js
@@ -1,5 +1,5 @@
 import cron from 'node-cron';
-import NotificationService from './services/NotificationService.js';
+import NotificationService from './notification.service.js';
 
 const notificationService = new NotificationService();
 
